f4e7aa86c01e5f6a7928af7cdb717d59de17b416
Sagarworkhub <sagarbarpete3@gmail.com>
2025-03-05 21:25:11 +0530

    add routing and sidebar layout to main application

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,13 +1,31 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import '../index.css';
 import App from '../App.tsx';
+import { Sidebar } from '../features/layout/components/sidebar.tsx';
+import { Dashboard } from './pages/dashboard.tsx';
+import { Setting } from './pages/setting.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route element={<Sidebar />}>
+          <Route
+            path='/'
+            element={<App />}
+          />
+          <Route
+            path='/dashboard'
+            element={<Dashboard />}
+          />
+          <Route
+            path='/setting'
+            element={<Setting />}
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
